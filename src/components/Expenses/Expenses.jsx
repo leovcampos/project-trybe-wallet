d52df4e362d756a1e74908c0439954680b623fac
leovcampos <leonardoviniciuscampos@gmail.com>
2022-08-02 17:55:40 -0300

    requisito-07

--- src/components/Expenses/Expenses.jsx
+++ src/components/Expenses/Expenses.jsx
@@ -1,10 +1,11 @@
+import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import './Expenses.css';
 import { connect } from 'react-redux';
 
 class Expenses extends Component {
   render() {
-    // const { expenses } = this.props;
+    const { expenses } = this.props;
     return (
       <table>
         <thead>
@@ -20,11 +21,42 @@ class Expenses extends Component {
             <th>Editar/Excluir</th>
           </tr>
         </thead>
-        <tbody />
+        <tbody>
+          {
+            expenses.map(({
+              description,
+              tag,
+              method,
+              value,
+              exchangeRates,
+              id,
+              currency,
+            }) => (
+              <tr
+                key={ id }
+              >
+                <td>{description}</td>
+                <td>{tag}</td>
+                <td>{method}</td>
+                <td>{Number(value).toFixed(2)}</td>
+                <td>{exchangeRates[currency].name}</td>
+                <td>{Math.round(exchangeRates[currency].ask * 100) / 100}</td>
+                <td>
+                  {Math.round((value * Number(exchangeRates[currency].ask) * 100)) / 100}
+                </td>
+                <td>Real</td>
+              </tr>
+            ))
+          }
+        </tbody>
       </table>
     );
   }
 }
+
+Expenses.propTypes = {
+  expenses: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
 
 const mapStateToProps = ({ wallet: { expenses } }) => ({
   expenses,
